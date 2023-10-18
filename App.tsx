2985d04b5e2d3a7b58c2a6acc930e6cdbc134d4d
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-10-19 01:53:42 +0300

    get react query back

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 // import { SheetProvider } from 'react-native-actions-sheet';
 
@@ -6,14 +6,14 @@ import './src/sheets/sheets';
 import HomePage from './src/screens/HomePage';
 
 export default function App() {
-  // const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
   return (
     <>
       {/* <SheetProvider> */}
-      {/* <QueryClientProvider client={queryClient}> */}
-      <HomePage />
-      {/* </QueryClientProvider> */}
+      <QueryClientProvider client={queryClient}>
+        <HomePage />
+      </QueryClientProvider>
       {/* </SheetProvider> */}
     </>
   );
